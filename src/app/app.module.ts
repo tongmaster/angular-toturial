05a020289e99acd87e35e11493597d18bdb928b3
tongmaster <basfers@gmail.com>
2020-01-26 16:56:30 +0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventBindingComponent } from './component/event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/event-binding/event-binding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-event-binding",
+  templateUrl: "./event-binding.component.html",
+  styleUrls: ["./event-binding.component.css"]
+})
+export class EventBindingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+  books: Array<string> = new Array<string>();
+  book: string = "";
+  add() {
+    this.books.push(this.book);
+    this.book = "";
+  }
+}
